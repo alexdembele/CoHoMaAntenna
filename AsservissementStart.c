6439e4be36c5ee1f7bae921eef6335af16e73428
alexdembele <43077858+alexdembele@users.noreply.github.com>
2023-06-13 10:22:15 +0200

    Update AsservissementStart.c

--- AsservissementStart.c
+++ AsservissementStart.c
@@ -1,21 +1,25 @@
 /*-------------Code arduino------------------
 @Dembélé Alex 
 
-Ce programme n'est pas fini. Il commence le code pour asservir les antennes avec des servomoteurs
+CE PROGRAMME N'EST PAS FINI. Il commence le code pour asservir les antennes avec des servomoteurs
 La communication avec avec l'opérateur/Interface Homme Machine est censé se faire avec rosArduino
 Pour apprendre en accéléré rosArduino, regardé les vidéos concernées du readME
 
-Attention: Régler initialisation des moteurs pour définir le 0 des angles*/
+Attention: Régler initialisation des moteurs pour définir le 0 des angles et peut-être prendre en compte l'angle initial par des soustractions dans l'asservissement
+Dans ce programme les angles initiaux des moteurs sont supposés être : -Direction-> 0° =  l'antenne pointe vers le Nord            -Inclinaison-> 0° = l'antenne est à l'horizontal
+
+Attention: Il y a du code commenté, notamment la partie rosArduino pour tester si le programme compile, cependant, il se peux qu'il y ai des erreurs de code restantes qui n'ont pas été corrigé*/
+
 
 
 
 
 #include <Servo.h>
 #include <math.h>
-/*#include <ros.h> // Cette bibliothèque est à installer si ce n'es pas déja fait. Cela peut se faire dans le programme arduino etsa gestion des bibliothèques. Il faut aussi que ros et pyserial soient installés sur votre ordinateur
+/*#include <ros.h> // Cette bibliothèque est à installer si ce n'es pas déja fait. Cela peut se faire dans le programme arduino et sa gestion des bibliothèques. Il faut aussi que ros et pyserial soient installés sur votre ordinateur
 #include <std_msgs/Int8.h> //Format des messages échangés (il faudra surement le changer pour s'échanger des listes de nombre
 
-ros::NodeHandle nh;
+ros::NodeHandle nh;                               //Pour tester la communication, la LED clignote si le programme reçoit le message 4
 void messageCb(const std_msgs::Int8& toggle_msg){
   if(toggle_msg.data==4)
   {
@@ -27,34 +31,36 @@ ros::Subscriber<std_msgs::Int8> sub("chatter", messageCb); //Subscriber pour com
 */
 Servo servoDirection;  // Servomoteur dirigeant la direction ("Nord Sud Est Ouest"), ou le lacet en langage maritime
 Servo servoInclinaison; // Servomoteur dirigeant l'inclinaison de l'antenne("Haut Bas")
+//Regarder setup() pour voir à quels pins sont attachés les moteurs
 
-float theta_direction;
-float theta_inclinaison;
 
-float x_vab;
+float theta_direction; //Angle de commande du moteur sur la direction
+float theta_inclinaison; //Angle de commande du moteur sur l'inclinaison
+
+float x_vab; //coordonnées du vab
 float y_vab;
 float z_vab;
 float angle_vab;
 
-float x_drone;
+float x_drone; //coordonnées du Tundra correspondant à cette antenne
 float y_drone;
 float z_drone;
 
-float x_husky;
+float x_husky; //coordonnées du Husky correspondant à cette antenne
 float y_husky;
 float z_husky;
 
 float hauteur ; //hauteur du robot
 float distance; //distance vab robot
-float angle_direction;
-float angle_inclinaison;
+float angle_direction; //angle de direction entre le VAB et le robot (ne prend pas en compte l'angle entre le VAB et le Nord)
+float angle_inclinaison; //angle d'inclinaison entre le VAB et le robot (ne prend pas en compte l'angle entre le VAB et l'horizontal)
 
 bool selection; //true focus drone, false focus husky
 
-float K_inclinaison; //gain asservissement
-float K_direction;
+float K_inclinaison; //gain asservissement sur l'inclinaison
+float K_direction;   //gain asservissement sur la direction
 
-float norme(float a_x, float a_y) //Fonction retournant la norme d'un vecteur a=(a_x, a__y). 
+float norme(float a_x, float a_y)           //Fonction retournant la norme d'un vecteur a=(a_x, a__y). 
 {
   return sqrt(a_x*a_x+a_y*a_y);
 }
@@ -76,16 +82,19 @@ float angleVecteur(float a_x, float a_y, float b_x, float b_y) // Détermine l'a
 }
 
 void setup() {
-  servoDirection.attach(9);  // attaches the servo on pin 9 to the servo object
-  servoInclinaison.attach(10);
+  servoDirection.attach(9);  //On attache le servoDirection sur le pin 9
+  servoInclinaison.attach(10); //On attache le servoInclinaison sur le pin 10
   pinMode(LED_BUILTIN, OUTPUT);
-  //nh.initNode();
+  
+  // Il faut faire l'initialisation de rosArduino
+  //nh.initNode(); 
   //nh.subscribe(sub);
 
 }
 
 void loop() {
-  //--Communication avec ordi--
+  //%%%%%%%%%%%%%%%%%%  Communication avec ordi %%%%%%%%%%%%%%%
+  //A faire
   //nh.spinOnce();
 
 
@@ -96,17 +105,18 @@ void loop() {
    
   //--selectionner target--
 
-  if(selection) //asservissement sur drone
+  
+  if(selection) //  --->DRONE
   {
-    //acquisition geometrie
+    //%%%%%%%%%%%%%%%%%%%%%%%% Calcul de la géométrie %%%%%%%%%%%%%%%
     hauteur=z_drone-z_vab;
     distance=norme(x_drone-x_vab,y_drone-y_vab);
     angle_inclinaison=atan(hauteur/distance);
-    angle_direction=angleVecteur(x_vab,y_vab,x_drone,y_drone); // limitée dans une plage angulaire par atan.
+    angle_direction=angleVecteur(x_vab,y_vab,x_drone,y_drone); 
     angle_direction-=angle_vab;
 
 
-    //Asservissement
+    // %%%%%%%%%%%%%%%%%%%%%% Asservissement %%%%%%%%%%%%%%%%%%%%%%%ù
 
     //theta_inclinaison+=K_inclinaison(angle_inclinaison-theta_inclinaison);
     //theta_direction+=K_direction(angle_direction-theta_direction);
@@ -115,10 +125,12 @@ void loop() {
     //servoInclinaison.write(theta_inclinaison);
 
   }
-
-  else
+  
+  
+ 
+  else // ---> HUSKY
   {
-    //acquisition geometrie
+    //%%%%%%%%%%%%%%%%%%%%%%%% Calcul de la géométrie %%%%%%%%%%%%%%%
     hauteur=z_husky-z_vab;
     distance=norme(x_husky-x_vab,y_husky-y_vab);
     angle_inclinaison=atan(hauteur/distance);
@@ -126,7 +138,7 @@ void loop() {
     angle_direction-=angle_vab;
 
 
-    //Asservissement
+    // %%%%%%%%%%%%%%%%%%ùAsservissement%%%%%%%%%%%%%%%%%%%%
 
     //theta_inclinaison+=K_inclinaison(angle_inclinaison-theta_inclinaison);
     //theta_direction+=K_direction(angle_direction-theta_direction);
